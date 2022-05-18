64a6c6b749299e871727680ee6333a572b3967d9
Timothy Yang <timothyoung97@gmail.com>
2022-05-18 12:38:19 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -15,32 +15,40 @@ project "GLFW"
         "src/input.c",
         "src/monitor.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+		"src/platform.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
     }
 
     filter "system:windows"
-    buildoptions { "-std=c11", "-lgdi32" }
-    systemversion "latest"
-    staticruntime "On"
+		buildoptions { "-std=c11", "-lgdi32" }
+		systemversion "latest"
+		staticruntime "On"
 
-    files
-    {
-        "src/win32_init.c",
-        "src/win32_joystick.c",
-        "src/win32_monitor.c",
-        "src/win32_time.c",
-        "src/win32_thread.c",
-        "src/win32_window.c",
-        "src/wgl_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c"
-    }
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_module.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.h",
+			"src/win32_time.c",
+			"src/win32_thread.h",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
 
-    defines
-    {
-        "_GLFW_WIN32",
-        "_CRT_SECURE_NO_WARNINGS"
-    }
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
 
     filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+		buildoptions "/MDd"
